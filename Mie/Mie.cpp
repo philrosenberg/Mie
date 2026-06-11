a4b8c9835581c96b989026d6735a8b74a977bd90
Phil Rosenberg <p.d.rosenberg@gmail.com>
2026-06-11 13:47:15 +0100

    changing some comments

--- Mie/Mie.cpp
+++ Mie/Mie.cpp
@@ -245,6 +245,9 @@ int main()
         return 1;
 
     //logarithmic derivative, called A in Wiscombe or D in Bohren anf Huffman
+    //In some circumstances it is stable to calculate this forward, but some 
+    //circumstances need us to calculate the last element, then work backwards.
+    //So we pre-calculate this to satisfy both scenarios
     std::vector<MieComplex>A(getLogarithmicDerivativesFastestStable(refractiveIndex, x, N));
 
 
@@ -395,14 +398,3 @@ int main()
             std::cout << "S2[" << i << "] failed\n";
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
